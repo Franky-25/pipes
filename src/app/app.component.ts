4cd9ca41506d7aaea81dd913854d57f850881bd4
Frank Bernales <f_bernales@hotmail.com>
2020-02-25 12:31:27 -0300

    Ultimo commit del curso pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { resolve } from 'dns';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @Component({
   selector: 'app-root',
@@ -7,11 +14,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  nombre = 'Fernando';
+  nombre = 'Frank';
   // arreglo = [1, 2, 3, 4, 5, 6, 7 , 8 , 9, 10];
+  nombre2: string = 'fraNK BErnalES';
   personajes: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 10000;
+  fecha: Date = new Date();
+  activar : boolean = true;
+
+  idioma: string = 'en';
+
+
+  valorPromesa = new Promise<string>(( resolve ) => {
+
+    setTimeout(() => {
+      resolve('llego la data')
+    }, 4500);
+
+  });
+
+  // propiedad-objeto
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
